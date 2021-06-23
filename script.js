6f0035b386a99bbe3fe3904c56b9f83e3dba9b56
GlebLyolis <86346891+GlebLyolis@users.noreply.github.com>
2021-06-23 13:45:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+
+function sayHello(){
+    
+    let myName = prompt('Как тебя зовут?');
+    let span = document.querySelector('span')
+    span.textContent = myName;
+}
+
+let button = document.querySelector('button')
+button.onclick(sayHello());
+
+*/
